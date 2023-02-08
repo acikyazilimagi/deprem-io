a9141f500a7e7623c5a31ed76a037535d406c543
Adem ilter <ademilter@gmail.com>
2023-02-09 01:53:36 +0300

    fix style

--- components/header.tsx
+++ components/header.tsx
@@ -13,7 +13,7 @@ export default function Header() {
       <div className="ml-auto">
         <Link
           href="/iletisim"
-          className="inline-flex h-10 items-center rounded-lg bg-blue-500 px-4 text-white"
+          className="inline-flex h-10 items-center rounded-lg bg-blue-600 px-4 text-white"
         >
           İletişim
         </Link>
