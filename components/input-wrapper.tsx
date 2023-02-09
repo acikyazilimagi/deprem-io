eb9868f55791108f9ffc67e8229c5d7c9395bb8a
Adem ilter <ademilter@gmail.com>
2023-02-09 14:11:46 +0300

    fix form style

--- components/input-wrapper.tsx
+++ components/input-wrapper.tsx
@@ -20,7 +20,7 @@ export default function InputWrapper({ children, icon }: InputProps) {
   return (
     <div className="relative">
       {icon && (
-        <span className="pointer-events-none absolute top-0 left-0 flex h-11 w-11 items-center justify-center opacity-60">
+        <span className="pointer-events-none absolute top-0 left-0 flex h-11 w-11 items-center justify-center opacity-60 dark:opacity-40">
           <Icon icon={icon} size={20} />
         </span>
       )}
