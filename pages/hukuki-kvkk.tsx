a778b700e291da9dad6a7dd01db501d6c95bf502
Adem ilter <ademilter@gmail.com>
2023-02-09 11:55:11 +0300

    kvkk

---
+++ pages/hukuki-kvkk.tsx
@@ -0,0 +1,164 @@
+import Head from "next/head";
+
+export default function HukukiKvkk() {
+  return (
+    <>
+      <Head>
+        <title>Hukuki ve KVKK - deprem.io</title>
+      </Head>
+
+      <h1>
+        KİŞİSEL VERİLERİN KORUNMASI VE İŞLENMESİ HAKKINDA AYDINLATMA VE RIZA
+        METNİ
+      </h1>
+
+      <ul className="mt-10 list-disc space-y-4">
+        <li>
+          deprem.io tarafından verilecek hizmetlerin gerektiği şekilde ve
+          güvenle sunulabilmesi için 6698 sayılı Kanun kapsamında kimliğinizi
+          belirli veya belirlenebilir kılan adınız, soyadınız, konumunuz, veli
+          ve vasi bilgisi, medeni hal, sağlık bilgisi ve mevcut durumunuz
+          kişisel veri olarak aşağıda belirtilen kapsamda, veri sorumlusu
+          sıfatıyla bu maddede anılan platformumuz tarafından işlenmekte olup,
+          kişisel verilerin elde edilmesi kaydedilmesi, depolanması, muhafaza
+          edilmesi, değiştirilmesi, yeniden düzenlenmesi, açıklanması,
+          aktarılması, devralınması, elde edilebilir hâle getirilmesi,
+          sınıflandırılması ya da kullanmasının engellenmesi işlenmesi ve üçüncü
+          kişilere aktarılması vb. gerekebilmektedir.
+        </li>
+        <li>
+          6698 sayılı Kişisel Verilerin Korunması Kanunu hükümlerine uygun
+          olarak, iş bu bilgilendirme formunda belirtilen hüküm ve koşullar
+          kapsamında işlenecek olan kişisel veriler; veri sahiplerine ait ad,
+          soyadı, konum, veli ve vasi bilgisi, sağlık durumu bilgisi ve mevcut
+          durumunuza ait bilgiyi kapsayacaktır.
+        </li>
+        <li>
+          Kişisel verilerin işlenmesi ve aktarılması 6698 sayılı Kişisel
+          Verilerin Korunması Kanunu hükümlerine uygun olarak
+          gerçekleştirilecektir. Bu kapsamda, kişisel verilerin hukuka ve
+          dürüstlük kuralına uygun, doğru ve güncel şekilde, belirli, açık ve
+          meşru amaçlar doğrultusunda işlenmesi ve aktarılması için her türlü
+          tedbir alınmaktadır.
+        </li>
+        <li>
+          Platformumuz , veri sahiplerine ait kişisel verilerin hukuka uygun
+          olarak toplanması, saklanması, işlenmesi ve gerekmesi durumunda
+          paylaşılması sırasında gizliliği korumak amacıyla mümkün olan en üst
+          seviyede güvenlik tedbirlerini almaktadır.
+        </li>
+        <li>
+          Platformumuz amacı; ilgilisi tarafından alenileştirilmiş veya
+          tarafımıza “deprem.io” adlı uygulamamız üzerinden ya da başka yollarla
+          bildirilmiş veri sahiplerine ait yukarıda sayılan kişisel verilerin
+          toplanma şekilleri, işlenme amaçları, işlemenin hukuki nedenleri ve
+          hakları konularında veri sahiplerini ve 06.02.2023 günü meydana gelen
+          deprem afeti kapsamında ilgili kamu kurum ve kuruluşlarını en şeffaf
+          şekilde bilgilendirmektir.
+        </li>
+        <li>
+          6698 sayılı Kişisel Verilerin Korunması Kanunu uyarınca veri
+          sahiplerine ait kişisel veriler, veri sorumlusu tarafından açıklanan
+          kapsamda toplanacak ve işlenebilecektir. Bu itibarla yukarıda sayılan
+          kişisel verileriniz, ilgili afetzedelerin sağlık, konum ve benzeri
+          durumlarının hızlı bir şekilde tespit edilerek yardım
+          ulaştırılabilmesi amacıyla 6698 sayılı Kanun’un 5. ve 6. maddelerinde
+          belirtilen kişisel veri işleme şartları ve amaçları dahilinde
+          işlenecektir. Platformumuza iletmiş olduğunuz veya platformumuzca
+          edinilen kişisel verilerinizin 6698 sayılı Kanun’un ilgili maddeleri
+          uyarınca işlenmesi hususunda platformumuza rıza vermiş
+          sayılmaktasınız. Verisi işlenen ilgililerin tarafımızla paylaştıkları
+          bilgilerin doğru olmasının ve güncel bir şekilde muhafaza edilmesinin
+          6698 sayılı Kanun anlamında kişisel verileri üzerinde sahip oldukları
+          hakları kullanabilmeleri ve ilgili diğer mevzuat açısından önemli
+          olduğunu bildiklerini ve yanlış bilgi verilmesinden doğacak
+          sorumlulukların tamamen kendilerine ait olacağını kabul ve beyan
+          etmişlerdir.
+        </li>
+        <li>
+          6698 sayılı Kişisel Verileri Koruma Kanunu'nun 5. ve 6.maddeleri
+          kapsamında yer alan ve özel nitelikli kişisel veri olarak kabul edilen
+          tüm veriler platformumuz tarafından ancak veri sahiplerinin açık
+          rızası ile ve iş bu metnin 5. maddesinde belirtilen amaçlarla sınırlı
+          olmak kaydıyla işlenebilir. Tarafımızca işlenen yukarıda açıklanan
+          kişisel veriler 6698 sayılı KVKK’ nın 7. Maddesinde belirtildiği
+          şekilde, ilgili verinin işlenmesini gerektiren sebeplerin ortadan
+          kalkması hâlinde kişisel veriler resen veya ilgili kişinin talebi
+          üzerine veri sorumlusu tarafından silinecektir. Veri sahiplerine ait
+          yukarıda sayılan kişisel veriler; kanunen yetkili ve ilgili kamu kurum
+          ve kuruluşları ile kanunen yetkili özel kuruluş/şirketlere 6698 sayılı
+          Kişisel Verilerin Korunması Kanunu’nun kişisel verilerin aktarılmasına
+          ilişkin 8. maddesi ve kişisel verilerin yurt dışına aktarılmasına
+          ilişkin 9. maddesinde belirtilen kişisel veri işleme şartları ve
+          amaçları çerçevesinde aktarılabilecektir.
+        </li>
+        <li>
+          Veri sahiplerine ait kişisel veriler; ilgili mevzuatın ve yapılan
+          anlaşmaların izin verdiği ölçüde ve çizdiği sınırlar dahilinde,
+          alenileştirilmiş veya kanunun 5/2 maddesinin b bendinde belirtilen
+          haller hariç olmak üzere hukuken zorunlu olduğu durumlarda veri
+          sahiplerinin onayı da alınmak suretiyle toplanacaktır. Bu yöntemle
+          toplanan kişisel veriler, 6698 sayılı Kişisel Verilerin Korunması
+          Kanunu’nun 5. ve 6. maddelerinde belirtilen kişisel veri işleme
+          şartları ve amaçları kapsamında ve yine iş bu bilgilendirmenin
+          5.maddesinde belirtilen amaçlar dahilinde işlenebilecektir.
+        </li>
+        <li>
+          6698 Sayılı Kişisel Verilerin Korunması Kanunu’nun 11. maddesindeki
+          düzenlemeye göre kişisel veri sahipleri; “Kişisel verilerinin işlenip
+          işlenmediğini öğrenme, kişisel verileri işlenmişse buna ilişkin bilgi
+          talep etme, kişisel verilerinin işlenme amacını ve bunların amacına
+          uygun kullanılıp kullanılmadığını öğrenme, yurt içinde veya yurt
+          dışında kişisel verilerinin aktarıldığı üçüncü kişileri bilme, kişisel
+          verilerinin eksik veya yanlış işlenmiş olması hâlinde bunların
+          düzeltilmesini isteme ve bu kapsamda yapılan işlemin kişisel verilerin
+          aktarıldığı üçüncü kişilere bildirilmesini isteme, 6698 sayılı Kişisel
+          Verilerin Korunması Kanunu ve ilgili diğer kanun hükümlerine uygun
+          olarak işlenmiş olmasına rağmen, işlenmesini gerektiren sebeplerin
+          ortadan kalkması halinde kişisel verilerinin silinmesini, yok
+          edilmesini veya anonim hâle getirilmesini isteme ve bu kapsamda
+          yapılan işlemin kişisel verilerinin aktarıldığı üçüncü kişilere
+          bildirilmesini isteme, işlenen verilerinin münhasıran otomatik
+          sistemler vasıtasıyla analiz edilmesi suretiyle aleyhine bir sonucun
+          ortaya çıktığını düşünüyor ise buna itiraz etme, kişisel verilerinin
+          kanuna aykırı olarak işlenmesi sebebiyle zarara uğramaları halinde
+          zararlarının giderilmesini talep etme hakları bulunmaktadır.
+        </li>
+        <li>
+          Veri sahipleri yukarıda bahsi geçen haklarını kullanmak üzere aşağıda
+          belirtilen usullerle deprem.io adresimize ulaştırabilecekleri gibi
+          yazılı başvurularını kendileri oluşturarak yine aşağıda belirtilen
+          usullerle deprem.io adresine ulaştırarak kullanabilirler. Başvurular,
+          ilgili veri sahibinin kimliğini tespit edecek belgelerle birlikte,
+          formun/başvurunun ıslak imzalı bir kopyasının elden veya noter
+          aracılığıyla ya da 6698 sayılı Kişisel Verileri Koruma Kanunu'nda
+          belirtilen diğer yöntemler ile yukarıda anılan platform adresine
+          iletilmesi veya 5070 sayılı Elektronik İmza Kanunu kapsamında
+          düzenlenen güvenli elektronik imza ile imzalanarak yukarıda anılan
+          e-mail adresine elektronik posta gönderimi ile
+          gerçekleştirilebilmektedir. Kişisel Verileri Koruma Kurulu tarafından
+          bahsi geçen yöntemler dışında bir yöntem öngörülmesi halinde
+          başvurular bu yöntemle de iletilebilecektir. Yukarıda belirtilen
+          yöntemlerden biri ile iletilen veri sahibi talepleri, platformumuz
+          tarafından azami otuz gün içerisinde değerlendirilmekte ve
+          cevaplanmaktadır. Tarafımız özellikle başvuru sahibinin ilgili veri
+          sahibi olup olmadığının değerlendirilmesi amacıyla başvuru sahibinden
+          ek bilgi ve belge talep etme hakkını saklı tutmaktadır. Veri sahibi
+          başvuruları kural olarak platformumuz tarafından ücretsiz olarak
+          değerlendirilmektedir. Ancak Kişisel Verileri Koruma Kurulu tarafından
+          veri sahibinin talebine ilişkin bir ücret belirlenmiş ise platformumuz
+          bu ücret üzerinden ödeme talep etme hakkı saklıdır.
+        </li>
+        <li>
+          Kimlik tespiti için gerekli bilgiler, cevap için tarafınıza
+          ulaşabileceğimiz telefon numarası, adres, e-posta adresiniz,
+          platformumuz ile olan ilişkiniz ve kapsamı, talep konunuz ve hangi
+          yollarla tarafınıza dönüş yapılmasını istediğinize yönelik
+          bilgilendirmeniz platformumuza yukarıda anılan yollarla
+          ulaştırıldıktan sonra ek kısa sürede ve en geç 30 gün içerisinde
+          tarafınıza cevap verilecektir.
+        </li>
+      </ul>
+    </>
+  );
+}
