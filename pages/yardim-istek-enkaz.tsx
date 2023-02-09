91d55909b8b6a2d19d04949008d3ed674069474d
Adem ilter <ademilter@gmail.com>
2023-02-09 12:02:41 +0300

    fix form item

--- pages/yardim-istek-enkaz.tsx
+++ pages/yardim-istek-enkaz.tsx
@@ -1,5 +1,6 @@
 import { Controller, useForm } from "react-hook-form";
 import { cx } from "@/lib/utils";
+import Link from "next/link";
 
 enum State {
   Kritik = "kritik",
@@ -125,12 +126,43 @@ export default function YardimIstekEnkaz() {
             />
           </div>
 
-          <button
-            type="submit"
-            className={cx(!formState.isValid && "opacity-50")}
-          >
-            Gönder
-          </button>
+          <div className="sm:col-span-2">
+            <p className="text-xs">
+              6698 sayılı KVKK kapsamında “Uygulamamıza depremzede ya da
+              depremzede yakını olarak kaydolan kullanıcılardan ad, soyadı,
+              iletişim bilgisi, log kaydı ve depremzedenin sisteme girilen ve
+              kendileri tarafından alenileştirilmiş konum verilerini
+              topluyoruz.” Veri işleme hukuki sebeplerimizi, amaçlarımızı görmek
+              ve haklarınızı öğrenmek için{" "}
+              <Link className="text-blue-500 underline" href="/hukuki-kvkk">
+                Aydınlatma Metnini
+              </Link>{" "}
+              ziyaret etmek ister misiniz?
+            </p>
+
+            <div className="mt-4">
+              <Controller
+                name="term"
+                control={control}
+                rules={{ required: true }}
+                render={({ field: { value, ...props } }) => (
+                  <label className="flex items-center gap-2">
+                    <input type="checkbox" checked={value} {...props} />
+                    <span>Okudum ve aydınlandım.</span>
+                  </label>
+                )}
+              />
+            </div>
+          </div>
+
+          <div>
+            <button
+              type="submit"
+              className={cx(!formState.isValid && "opacity-50")}
+            >
+              Gönder
+            </button>
+          </div>
         </div>
       </form>
     </div>
