94915277b63fba7eadd7beeb8d8803edf98843b9
kbuzcu <kbuzcu@kazandirio-dev.com>
2023-02-10 19:28:33 +0300

    fix: lint error

--- lib/validations/index.ts
+++ lib/validations/index.ts
@@ -1,5 +1,5 @@
 import * as yup from "yup";
 
-import { LOCALE_VALIDATIONS } from "./localeValidations.ts";
+import { LOCALE_VALIDATIONS } from "./localeValidations";
 
 export const setLocaleValidation = () => yup.setLocale(LOCALE_VALIDATIONS);
