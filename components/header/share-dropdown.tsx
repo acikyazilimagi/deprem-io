1f118f474f26c20e3d8fafe0a373cddb4b259891
berkcanozbalci966 <berkcanozbalci@gmail.com>
2023-02-11 20:25:16 +0300

    feat: share div on hover background color

--- components/header/share-dropdown.tsx
+++ components/header/share-dropdown.tsx
@@ -7,11 +7,13 @@ export default function ShareDropdown() {
   const [isDropdownOpen, toggleDropdown] = useToggle()
   const dropdownRef = useRef<HTMLDivElement>(null)
 
-  useOnClickOutside(dropdownRef, () => {if(isDropdownOpen) toggleDropdown()})
+  useOnClickOutside(dropdownRef, () => {
+    if (isDropdownOpen) toggleDropdown()
+  })
 
   return (
     <div
-      className="flex justify-center"
+      className="flex justify-center rounded-lg px-5 py-2.5 hover:bg-zinc-700"
       ref={dropdownRef}
       onClick={() => {
         toggleDropdown()
@@ -53,17 +55,17 @@ export default function ShareDropdown() {
           m-0
           mt-1
            ${!isDropdownOpen && 'hidden'}
+          flex
           min-w-max
           list-none
           rounded-lg
           border-none
+          bg-zinc-400
           bg-clip-padding
           py-2
           text-left
           text-base
           shadow-lg
-          bg-zinc-400
-          flex
         `}
             aria-labelledby="dropdownMenuButton2"
           >
@@ -82,7 +84,8 @@ export default function ShareDropdown() {
               text-gray-700
               hover:bg-gray-100
             "
-                href="https://www.addtoany.com/add_to/email?linkurl=https%3A%2F%2Fdeprem.io&linkname=Yard%C4%B1m%20iste%20%26%20yard%C4%B1m%20sa%C4%9Fla.%20Deprem%20%C4%B0mece%20Platformu%20-%20deprem.io&linknote=" rel="noreferrer"
+                href="https://www.addtoany.com/add_to/email?linkurl=https%3A%2F%2Fdeprem.io&linkname=Yard%C4%B1m%20iste%20%26%20yard%C4%B1m%20sa%C4%9Fla.%20Deprem%20%C4%B0mece%20Platformu%20-%20deprem.io&linknote="
+                rel="noreferrer"
               >
                 <Icon icon="email" />
               </a>
@@ -102,10 +105,11 @@ export default function ShareDropdown() {
               text-gray-700
               hover:bg-gray-100
             "
-                target='_blank'
-                href="https://www.addtoany.com/add_to/facebook?linkurl=https%3A%2F%2Fdeprem.io&linkname=Yard%C4%B1m%20iste%20%26%20yard%C4%B1m%20sa%C4%9Fla.%20Deprem%20%C4" rel="noreferrer"
+                target="_blank"
+                href="https://www.addtoany.com/add_to/facebook?linkurl=https%3A%2F%2Fdeprem.io&linkname=Yard%C4%B1m%20iste%20%26%20yard%C4%B1m%20sa%C4%9Fla.%20Deprem%20%C4"
+                rel="noreferrer"
               >
-               <Icon icon="facebook" />
+                <Icon icon="facebook" />
               </a>
             </li>
             <li>
@@ -123,10 +127,11 @@ export default function ShareDropdown() {
               text-gray-700
               hover:bg-gray-100
             "
-                target='_blank'
-                href="https://www.addtoany.com/add_to/twitter?linkurl=https%3A%2F%2Fdeprem.io&linkname=Yard%C4%B1m%20iste%20%26%20yard%C4%B1m%20sa%C4%9Fla.%20Deprem%20%C4%B0mece%20Platformu%20-%20deprem.io&linknote=" rel="noreferrer"
+                target="_blank"
+                href="https://www.addtoany.com/add_to/twitter?linkurl=https%3A%2F%2Fdeprem.io&linkname=Yard%C4%B1m%20iste%20%26%20yard%C4%B1m%20sa%C4%9Fla.%20Deprem%20%C4%B0mece%20Platformu%20-%20deprem.io&linknote="
+                rel="noreferrer"
               >
-               <Icon icon="twitter" />
+                <Icon icon="twitter" />
               </a>
             </li>
             <li>
@@ -144,31 +149,11 @@ export default function ShareDropdown() {
               text-gray-700
               hover:bg-gray-100
             "
-                target='_blank'
-                href="https://www.addtoany.com/add_to/linkedin?linkurl=https%3A%2F%2Fdeprem.io&linkname=Yard%C4%B1m%20iste%20%26%20yard%C4%B1m%20sa%C4%9Fla.%20Deprem%20%C4%B0mece%20Platformu%20-%20deprem.io&linknote=" rel="noreferrer"
+                target="_blank"
+                href="https://www.addtoany.com/add_to/linkedin?linkurl=https%3A%2F%2Fdeprem.io&linkname=Yard%C4%B1m%20iste%20%26%20yard%C4%B1m%20sa%C4%9Fla.%20Deprem%20%C4%B0mece%20Platformu%20-%20deprem.io&linknote="
+                rel="noreferrer"
               >
-               <Icon icon="linkedin" />
-              </a>
-            </li>
-                        <li>
-              <a
-                className="
-              dropdown-item
-              block
-              w-full
-              whitespace-nowrap
-              bg-transparent
-              py-2
-              px-4
-              text-sm
-              font-normal
-              text-gray-700
-              hover:bg-gray-100
-            "
-                target='_blank'
-                href="https://www.addtoany.com/add_to/tumblr?linkurl=https%3A%2F%2Fdeprem.io&linkname=Yard%C4%B1m%20iste%20%26%20yard%C4%B1m%20sa%C4%9Fla.%20Deprem%20%C4%B0mece%20Platformu%20-%20deprem.io&linknote=" rel="noreferrer"
-              >
-               <Icon icon="tumblr" />
+                <Icon icon="linkedin" />
               </a>
             </li>
             <li>
@@ -186,10 +171,33 @@ export default function ShareDropdown() {
               text-gray-700
               hover:bg-gray-100
             "
-                target='_blank'
-                href="https://www.addtoany.com/add_to/reddit?linkurl=https%3A%2F%2Fdeprem.io&linkname=Yard%C4%B1m%20iste%20%26%20yard%C4%B1m%20sa%C4%9Fla.%20Deprem%20%C4%B0mece%20Platformu%20-%20deprem.io&linknote=" rel="noreferrer"
+                target="_blank"
+                href="https://www.addtoany.com/add_to/tumblr?linkurl=https%3A%2F%2Fdeprem.io&linkname=Yard%C4%B1m%20iste%20%26%20yard%C4%B1m%20sa%C4%9Fla.%20Deprem%20%C4%B0mece%20Platformu%20-%20deprem.io&linknote="
+                rel="noreferrer"
               >
-               <Icon icon="reddit" />
+                <Icon icon="tumblr" />
+              </a>
+            </li>
+            <li>
+              <a
+                className="
+              dropdown-item
+              block
+              w-full
+              whitespace-nowrap
+              bg-transparent
+              py-2
+              px-4
+              text-sm
+              font-normal
+              text-gray-700
+              hover:bg-gray-100
+            "
+                target="_blank"
+                href="https://www.addtoany.com/add_to/reddit?linkurl=https%3A%2F%2Fdeprem.io&linkname=Yard%C4%B1m%20iste%20%26%20yard%C4%B1m%20sa%C4%9Fla.%20Deprem%20%C4%B0mece%20Platformu%20-%20deprem.io&linknote="
+                rel="noreferrer"
+              >
+                <Icon icon="reddit" />
               </a>
             </li>
           </ul>
