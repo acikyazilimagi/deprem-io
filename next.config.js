6ea6219dde7178b3dcdc2a7bc5d38b40dbbe8141
agtt <csd@hotmail.com.tr>
2023-02-11 11:10:20 +0000

    feature/pwa - Progressive Web App - offline support

--- next.config.js
+++ next.config.js
@@ -1,8 +1,19 @@
 const withNextTranslate = require('next-translate')
+const runtimeCaching = require('next-pwa/cache')
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
 }
 
-module.exports = withNextTranslate(nextConfig)
+// PWA Config
+const withPWA = require('next-pwa')({
+  dest: 'public',
+  register: true,
+  skipWaiting: true,
+  runtimeCaching,
+  buildExcludes: [/middleware-manifest.json$/],
+  disable: process.env.NODE_ENV === 'development',
+})
+
+module.exports = withNextTranslate(withPWA(nextConfig))
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -4,6 +4,9 @@ export default function Document() {
   return (
     <Html lang="en" className="antialiased">
       <Head />
+      <link rel="manifest" href="/manifest.json" />
+      <link rel="apple-touch-icon" href="/icon.png" />
+      <meta name="theme-color" content="#042940" />
       <body className="bg-white text-zinc-900 dark:bg-zinc-900 dark:text-white">
         <Main />
         <NextScript />
