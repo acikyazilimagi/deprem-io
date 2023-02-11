b4c8fb8341e205767c4f2aab9293c822142e81fc
agtt <csd@hotmail.com.tr>
2023-02-11 11:30:49 +0000

    PWA tags moved to head

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -3,10 +3,11 @@ import { Head, Html, Main, NextScript } from 'next/document'
 export default function Document() {
   return (
     <Html lang="en" className="antialiased">
-      <Head />
-      <link rel="manifest" href="/manifest.json" />
-      <link rel="apple-touch-icon" href="/icon.png" />
-      <meta name="theme-color" content="#042940" />
+      <Head>
+        <link rel="manifest" href="/manifest.json" />
+        <link rel="apple-touch-icon" href="/icon.png" />
+        <meta name="theme-color" content="#042940" />
+      </Head>
       <body className="bg-white text-zinc-900 dark:bg-zinc-900 dark:text-white">
         <Main />
         <NextScript />
